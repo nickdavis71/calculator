de4cc970ac70123fcdedd29e0325b342b2bd7056
Nick Davis <nickdavis71@gmail.com>
2023-04-11 22:40:49 -0600

    Created JavaScript math functions and HTML buttons

---
+++ calculator.js
@@ -0,0 +1,38 @@
+let a = 3;
+let b = 5;
+let operator = '+';
+
+
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    };
+};
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+console.log(operate(a, b, operator));
